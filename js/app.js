74d6eb6ff5b9e2e48a32d65b5c48db85971da612
AlyssaD <pink_lime@live.com>
2022-09-09 14:42:12 -0400

    initial HTML

--- js/app.js
+++ js/app.js
@@ -15,3 +15,23 @@
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+
+
+
+//  pseudo-code
+
+// - add HTML for game board (div’s and buttons)
+// - define variables for img, current-bet, 
+// - create icon array
+// - store cached elements for message, buttons, img
+// - create function to initialize the game
+// - create render function
+// - click events for betting
+// - create function to place bet
+// - create function to randomize the icon array
+// - create function to play the game
+// - create function to determine winner/payout
+// - animation for icon display at different intervals
+// - create game reset
+// - sounds/animation for jackpot
